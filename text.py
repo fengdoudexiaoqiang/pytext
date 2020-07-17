fd0fe71bd22fb11f964d4b0910f9d98973ce8be7
Chen <18702069517@163.com>
2020-07-17 11:41:35 +0800

    test1

--- text.py
+++ text.py
@@ -19,3 +19,10 @@ class Test_allure:
     @allure.step('我是测试步骤001')
     def test_al(self, a):
         assert a != 2
+
+    @pytest.issue('http://www.163.com')
+    @pytest.allure.testcase('http://www.baidu.com/test_001')
+    @allure.allure.severity(pytest.allure.severity_leverl.TRIVIAL)
+    def test_al01(self):
+        allure.attach('我是一个描述')
+        assert 0
